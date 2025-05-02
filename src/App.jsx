74938b594b1bdfbc0805c0f29d78e281e15baea8
Sammy G <78850919+sam-gtz@users.noreply.github.com>
2025-05-01 21:29:04 -0700

    Creating Page rendering Flow for prepping AnalyticsPage

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AnalyticsPage from "./pages/AnalyticsPage";
+
+export default function App() {
+    return <>
+        <h1>Hello Vite</h1>
+        <AnalyticsPage />
+    
+    </>
+    
+    
+}
---
+++ src/pages/AnalyticsPage.jsx
@@ -0,0 +1,17 @@
+
+import Header from '../components/common/Header'
+
+
+
+const AnalyticsPage = () => {
+    return (
+
+        <>
+           <Header title={"You Did it!"} /> 
+        </>
+
+    )
+};
+
+
+export default AnalyticsPage;
